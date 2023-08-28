a8bb5e570960ba4f337f70fd1993fb1ff2b68763
Jordy Caichihua <jcaichihuav@gmail.com>
2023-08-28 18:56:54 -0500

    add style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  /* templateUrl: './app.component.html', */
+  template:`
+  <h1>Curso de Angular</h1>
+  `,
+  /* styleUrls: ['./app.component.css'] */
+  styles: [`
+  `]
 })
 export class AppComponent {
   title = 'curso_angular';
